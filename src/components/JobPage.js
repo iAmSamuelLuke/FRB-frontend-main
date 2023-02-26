4e4475c3aca5d8305359f4fbe16166aeeb17302f
ChessMathNerd <pjm61300@ucmo.edu>
2023-02-25 20:29:42 -0600

    Working on jobs

---
+++ src/components/JobPage.js
@@ -0,0 +1,14 @@
+import Job from "./elements/Job"
+
+const JobPage = () => {
+    return (
+        <div>
+            <Job 
+                title='Application Developer'
+                experience
+            />
+        </div>
+    );
+}
+
+export default JobPage;
